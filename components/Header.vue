b6003ef092b8b0546d76697f42f540a16ed4b348
KyungMin Lee <42348176+Leekm0912@users.noreply.github.com>
2022-12-13 18:20:09 +0900

    defualt setting (#1)

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header_wrap">
+    <header class="header">
+      <h1 class="logo">
+        <NuxtLink to="/">
+          <a href>
+            <img src="~/assets/img/rcs_logo.png" alt="RCS Biz Center">
+          </a>
+        </NuxtLink>
+      </h1>
+
+      <div class="util_wrap">
+        <NuxtLink to="/helloWorld">hello</NuxtLink>
+        <NuxtLink to="/ImageView">imageView</NuxtLink>
+        <a href="javascript:void(0);" class="btn_setting">정보수정</a>
+        <a href="javascript:void(0);" class="btn_logout">로그아웃</a>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  computed: {
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>mainPage</h1>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
